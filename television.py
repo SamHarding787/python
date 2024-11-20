acd8df69580eab7797d7c7f1bb39cd063c1bea10
SamHarding787 <samharding@unomaha.edu>
2024-11-20 14:26:34 -0600

    started on coding

---
+++ television.py
@@ -0,0 +1,29 @@
+
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        """
+
+        """
+        status = False
+        muted = False
+        volume = 0
+        channel = 0
+
+    def power(self):
+
+    def mute(self):
+
+    def channel_up(self):
+
+    def channel_down(self):
+
+    def volume_up(self):
+
+    def volume_down(self):
+
+    def __str__(self):
