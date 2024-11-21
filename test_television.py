729fc40b49712db1a82bec22b2241a8aff25be40
SamHarding787 <samharding@unomaha.edu>
2024-11-20 19:51:02 -0600

    created test file and created def

--- test_television.py
+++ test_television.py
@@ -1,15 +1,22 @@
 from television import Television
 
 def test_init():
+    tv = Television()
 
 def test_power():
+    tv = Television()
 
 def test_channel_up():
+    tv = Television()
 
 def test_channel_down():
+    tv = Television()
 
 def test_volume_up():
+    tv = Television()
 
 def test_volume_down():
+    tv = Television()
 
 def test_mute():
+    tv = Television()
