2a6e372a800a07b8b57da6729c0d977c01d257ca
SamHarding787 <samharding@unomaha.edu>
2024-11-20 19:50:00 -0600

    created test file and created def

--- test_television.py
+++ test_television.py
@@ -1,1 +1,15 @@
-print("THE BODIES ARE HITTING THE FLOOR")
+from television import Television
+
+def test_init():
+
+def test_power():
+
+def test_channel_up():
+
+def test_channel_down():
+
+def test_volume_up():
+
+def test_volume_down():
+
+def test_mute():
