7734864b98d56072e775586f13402a4e43d5c4ba
SamHarding787 <samharding@unomaha.edu>
2024-11-26 14:01:08 -0600

    made variables private and wrote a ton of comments

--- television.py
+++ television.py
@@ -7,48 +7,48 @@ class Television:
 
     def __init__(self):
         """Initializes the TV. Uses default values."""
-        self._status = False
-        self._muted = False
-        self._volume = self.MIN_VOLUME
-        self._channel = self.MIN_CHANNEL
+        self.__status = False
+        self.__muted = False
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
 
     def power(self):
         """Changes whether the TV is on or not."""
-        self._status = not self._status
+        self.__status = not self.__status  # Flips what is happening
 
     def mute(self):
         """Toggles the mute status of the TV."""
-        if self._status:
-            self._muted = not self._muted
+        if self.__status:  # Checks if the tv is on
+            self.__muted = not self.__muted # Flips what is happening
 
     def channel_up(self):
         """Changes the channel up by 1."""
-        if self._status:
-            self._channel = (self._channel + 1) % (self.MAX_CHANNEL + 1)
+        if self.__status: # Checks if the tv is on
+            self.__channel = (self.__channel + 1) % (self.MAX_CHANNEL + 1) # Basically adds one to total
 
     def channel_down(self):
         """Changes the channel down by 1."""
-        if self._status:
-            self._channel = (self._channel - 1) % (self.MAX_CHANNEL + 1)
+        if self.__status: # Checks if the tv is on
+            self.__channel = (self.__channel - 1) % (self.MAX_CHANNEL + 1) # Decreases total by one
 
     def volume_up(self):
         """Increases the volume by 1."""
-        if self._status:
-            if self._muted:
-                self._muted = False
-            if self._volume < self.MAX_VOLUME:
-                self._volume += 1
+        if self.__status: # Checks if the tv is on
+            if self.__muted: # Checks to see if its muted
+                self.__muted = False
+            if self.__volume < self.MAX_VOLUME:  # Basically adds one to total
+                self.__volume += 1
 
     def volume_down(self):
         """Decreases the volume by 1."""
-        if self._status:
-            if self._muted:
-                self._muted = False
-            if self._volume > self.MIN_VOLUME:
-                self._volume -= 1
+        if self.__status: # Checks if the tv is on
+            if self.__muted: # Checks to see if its muted
+                self.__muted = False
+            if self.__volume > self.MIN_VOLUME:  # Decreases total by one
+                self.__volume -= 1
 
     def __str__(self):
         """Gives the print statement for the current TV status."""
-        status = "True" if self._status else "False"
-        volume = 0 if self._muted else self._volume
-        return f"Power = {status}, Channel = {self._channel}, Volume = {volume}"
+        status = "True" if self.__status else "False"
+        volume = 0 if self.__muted else self.__volume
+        return f"Power = {status}, Channel = {self.__channel}, Volume = {volume}"
