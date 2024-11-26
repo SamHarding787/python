b8dcf7c4818ef64dddb3f1e12aed1ee80c1dbd64
SamHarding787 <samharding@unomaha.edu>
2024-11-26 14:03:37 -0600

    made variables private and wrote a ton of comments

--- television.py
+++ television.py
@@ -12,26 +12,26 @@ class Television:
         self.__volume = self.MIN_VOLUME
         self.__channel = self.MIN_CHANNEL
 
-    def power(self):
+    def power(self): # Turns the tv off and on
         """Changes whether the TV is on or not."""
         self.__status = not self.__status  # Flips what is happening
 
-    def mute(self):
+    def mute(self): # Mutes the TV
         """Toggles the mute status of the TV."""
         if self.__status:  # Checks if the tv is on
             self.__muted = not self.__muted # Flips what is happening
 
-    def channel_up(self):
+    def channel_up(self): # Turns the channel up by 1
         """Changes the channel up by 1."""
         if self.__status: # Checks if the tv is on
             self.__channel = (self.__channel + 1) % (self.MAX_CHANNEL + 1) # Basically adds one to total
 
-    def channel_down(self):
+    def channel_down(self): # Turns the channel down by 1
         """Changes the channel down by 1."""
         if self.__status: # Checks if the tv is on
             self.__channel = (self.__channel - 1) % (self.MAX_CHANNEL + 1) # Decreases total by one
 
-    def volume_up(self):
+    def volume_up(self): # Turns the volume up by 1
         """Increases the volume by 1."""
         if self.__status: # Checks if the tv is on
             if self.__muted: # Checks to see if its muted
@@ -39,7 +39,7 @@ class Television:
             if self.__volume < self.MAX_VOLUME:  # Basically adds one to total
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self): # Turns the volume down by 1
         """Decreases the volume by 1."""
         if self.__status: # Checks if the tv is on
             if self.__muted: # Checks to see if its muted
