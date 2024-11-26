dd7d5a265219d4c1fe2e2465d2da9f0824720cd1
SamHarding787 <samharding@unomaha.edu>
2024-11-26 14:11:15 -0600

    made variables private and wrote a ton of comments again

--- test_television.py
+++ test_television.py
@@ -6,75 +6,75 @@ class TestTelevision(unittest.TestCase):
     def test_init(self):
         """Testing the init"""
         tv = Television()
-        self.assertFalse(tv._status, "TV should start off.")
-        self.assertFalse(tv._muted, "TV should start unmuted.")
-        self.assertEqual(tv._volume, Television.MIN_VOLUME, "Volume should start at minimum.")
-        self.assertEqual(tv._channel, Television.MIN_CHANNEL, "Channel should start at minimum.")
+        self.assertFalse(tv.__status, "TV should start off.")
+        self.assertFalse(tv.__muted, "TV should start unmuted.")
+        self.assertEqual(tv.__volume, Television.MIN_VOLUME, "Volume should start at minimum.")
+        self.assertEqual(tv.__channel, Television.MIN_CHANNEL, "Channel should start at minimum.")
 
     def test_power(self):
         """Testing power"""
         tv = Television()
-        tv.power()
-        self.assertTrue(tv._status, "TV should be on after calling power.")
-        tv.power()
-        self.assertFalse(tv._status, "TV should be off after calling power again.")
+        tv.power() # Turn on the tv
+        self.assertTrue(tv.__status, "TV should be on after calling power.")
+        tv.power() # Turn off the tv
+        self.assertFalse(tv.__status, "TV should be off after calling power again.")
 
     def test_channel_up(self):
         """Testing channel_up"""
         tv = Television()
         tv.power()  # Turn on the TV
-        tv.channel_up()
+        tv.channel_up() # Change up the channel
         self.assertEqual(tv._channel, 1, "Channel should increment by 1.")
         tv._channel = Television.MAX_CHANNEL
-        tv.channel_up()
+        tv.channel_up()  # Change up the channel
         self.assertEqual(tv._channel, Television.MIN_CHANNEL, "Channel should wrap to minimum.")
 
     def test_channel_down(self):
         """Testing channel_down"""
         tv = Television()
         tv.power()  # Turn on the TV
-        tv.channel_down()
+        tv.channel_down() # Change down the channel
         self.assertEqual(tv._channel, Television.MAX_CHANNEL, "Channel should wrap to maximum.")
         tv._channel = 1
-        tv.channel_down()
+        tv.channel_down() # Change down the channel
         self.assertEqual(tv._channel, 0, "Channel should decrement by 1.")
 
     def test_volume_up(self):
         """Testing volume_up"""
         tv = Television()
         tv.power()  # Turn on the TV
-        tv.volume_up()
+        tv.volume_up() # Change up the volume
         self.assertEqual(tv._volume, 1, "Volume should increment by 1.")
         tv._volume = Television.MAX_VOLUME
-        tv.volume_up()
+        tv.volume_up() # Change up the volume
         self.assertEqual(tv._volume, Television.MAX_VOLUME, "Volume should stay at maximum.")
 
     def test_volume_down(self):
         """Testing volume_down"""
         tv = Television()
         tv.power()  # Turn on the TV
-        tv.volume_down()
+        tv.volume_down() # Change down the volume
         self.assertEqual(tv._volume, Television.MIN_VOLUME, "Volume should stay at minimum.")
         tv._volume = 1
-        tv.volume_down()
+        tv.volume_down() # Change down the volume
         self.assertEqual(tv._volume, 0, "Volume should decrement by 1.")
 
     def test_mute(self):
         """Testing mute"""
         tv = Television()
         tv.power()  # Turn on the TV
-        tv.volume_up()
-        self.assertEqual(tv._volume, 1, "Volume should be 1 before muting.")
-        tv.mute()
-        self.assertTrue(tv._muted, "TV should be muted.")
-        tv.mute()
-        self.assertFalse(tv._muted, "TV should be unmuted.")
+        tv.volume_up() # Change up the volume
+        self.assertEqual(tv.__volume, 1, "Volume should be 1 before muting.")
+        tv.mute() # Mutes the tv
+        self.assertTrue(tv.__muted, "TV should be muted.")
+        tv.mute() # Mutes the tv
+        self.assertFalse(tv.__muted, "TV should be unmuted.")
 
     def test_str_method(self):
         """Test the __str__ method."""
         tv = Television()
         self.assertEqual(str(tv), "Power = False, Channel = 0, Volume = 0", "String representation should match initial state.")
-        tv.power()
-        tv.volume_up()
-        tv.channel_up()
+        tv.power()  # Turn on the TV
+        tv.volume_up() # Change up the volume
+        tv.channel_up()  # Change up the channel
         self.assertEqual(str(tv), "Power = True, Channel = 1, Volume = 1", "String representation should match current state.")
